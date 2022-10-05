945e4abac386be88343ebf9c01030c0ccf135d6a
oanhlewkm <oanhle@wakumo.vn>
2022-10-05 14:42:44 +0700

    finish add and remove favorite list

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 12);
-
+  final _saved = <WordPair>{};
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -49,11 +49,25 @@ class _RandomWordsState extends State<RandomWords> {
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10)); /*4*/
           }
+          final alreadySaved = _saved.contains(_suggestions[index]);
           return ListTile(
             title: Text(
               _suggestions[index].asPascalCase,
               style: _biggerFont,
             ),
+            trailing: Icon(
+              alreadySaved ? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved ? Colors.red : null,
+              semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+            ),
+            onTap: () => {
+              setState(() => {
+                    if (alreadySaved)
+                      {_saved.remove(_suggestions[index])}
+                    else
+                      {_saved.add(_suggestions[index])}
+                  })
+            },
           );
         });
   }
